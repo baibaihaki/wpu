1377cd8945d33e41a6f7196a94e7925ae1c54f43
baibaihaki <baihakibbaaii@gmail.com>
2026-07-19 07:28:25 +0700

    perbarui tampilan dan pesan selamat datang di index.php

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -1,12 +1,17 @@
+<?php
+$nama = "Baihaki";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Pertemuan 2</title>
 </head>
 <body>
-  <?php echo "Selamat Pagi Baihaki"?>
+  <h1>Selamat datang <?php echo $nama; ?></h1>
   <?php echo "Selamat menjalankan tugas anda di hari ini"?>
   <?php echo "Ada hal yang bisa dibantu?"?>
 </body>
